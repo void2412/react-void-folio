24d80ba29f82463f92db5a58a90bf611c2fb7a36
void2412 <void2412@gmail.com>
2022-08-01 19:49:46 +0930

    added project data

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,49 +1,54 @@
 import React from "react";
 import Project from "./Project"
 import passwordGen from "../images/passwordGen.gif"
+import myButcher from "../images/myButcher.jpg"
+import noteTaker from "../images/noteTaker.png"
+import taskPlanner from "../images/TaskPlanner.png"
+import techBlog from "../images/techBlog.gif"
+import textEditor from "../images/Text Editor.png"
 function Portfolio(){
 	const projects = [
 		{
 			key:"1",
 			imagePath: passwordGen,
-			githubUrl: "",
-			deployedUrl: "",
+			githubUrl: "https://github.com/void2412/passwordGenerator",
+			deployedUrl: "https://void2412.github.io/passwordGenerator/",
 			title:"Password Generator"
 		},
 		{
 			key:"2",
-			imagePath: "",
-			githubUrl: "",
-			deployedUrl: "",
-			title:""
+			imagePath: myButcher,
+			githubUrl: "https://github.com/void2412/MyButcher",
+			deployedUrl: "https://ordering-system-team4.herokuapp.com/",
+			title:"My Butcher"
 		},
 		{
 			key:"3",
-			imagePath: "",
-			githubUrl: "",
-			deployedUrl: "",
-			title:""
+			imagePath: noteTaker,
+			githubUrl: "https://github.com/void2412/noteTaker",
+			deployedUrl: "https://note-taker-2412.herokuapp.com/",
+			title:"Note Taker"
 		},
 		{
 			key:"4",
-			imagePath: "",
-			githubUrl: "",
-			deployedUrl: "",
-			title:""
+			imagePath: taskPlanner,
+			githubUrl: "https://github.com/void2412/Task-planner",
+			deployedUrl: "https://void2412.github.io/Task-planner/",
+			title:"Task Planner"
 		},
 		{
 			key:"5",
-			imagePath: "",
-			githubUrl: "",
-			deployedUrl: "",
-			title:""
+			imagePath: techBlog,
+			githubUrl: "https://github.com/void2412/techBlog",
+			deployedUrl: "https://tech-blog-2412.herokuapp.com/",
+			title:"Tech Blog"
 		},
 		{
 			key:"6",
-			imagePath: "",
-			githubUrl: "",
-			deployedUrl: "",
-			title:""
+			imagePath: textEditor,
+			githubUrl: "https://github.com/void2412/Text-Editor",
+			deployedUrl: "https://jate-void.herokuapp.com/",
+			title:"Text Editor"
 		}
 	]
 	return (
--- src/components/project.js
+++ src/components/project.js
@@ -5,12 +5,12 @@ function Project(props){
 	// image, title, link to deployed, link to github
 	return(
 		<div className="card col-md-12 col-lg-6 border-0">
-			<img className="card-img-top" src={props.data.imagePath} alt=""></img>
+			<img className="card-img-top" src={props.data.imagePath} alt="" ></img>
 			<div className="card-body text-center">
 				<h5 className="card-title">{props.data.title}</h5>
 				<div className="btn-group">
-					<a className="btn btn-primary" href={props.data.deployedUrl}>Deployed</a>
-					<a className="btn btn-primary" href={props.data.githubUrl}>Github</a>
+					<a className="btn btn-primary" href={props.data.deployedUrl} target="_blank" rel="noreferrer noopener">Deployed</a>
+					<a className="btn btn-primary" href={props.data.githubUrl} target="_blank" rel="noreferrer noopener">Github</a>
 				</div>
 			</div>
 		</div>
